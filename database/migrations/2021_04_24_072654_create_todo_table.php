e0d1e22e03961d724cc67e3a91ca3f20583241dd
dikshant rajput <dikshantraj2001@gmail.com>
2021-04-24 13:16:32 +0530

    initializing app

---
+++ database/migrations/2021_04_24_072654_create_todo_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTodoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todo', function (Blueprint $table) {
+            $table->id();
+            $table->string('created_by');
+            $table->string('todo_text');
+            $table->dateTime('end_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todo');
+    }
+}
